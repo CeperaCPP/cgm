38e2bda79bb57dffc4c9efd340929ed96350e798
CeperaCPP <CeperaCPP@gmail.com>
2015-08-08 14:35:46 +0300

    Добавлен начальный исходный код проекта

---
+++ cgm/mainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cpm
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripRight_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+        //=====================================================================
+        /// Изменение размера функциональных клавиш
+        //=====================================================================
+        private void toolStripBottom_Resize(object sender, EventArgs e)
+        {
+            //MessageBox.Show("hello");
+        }
+        //=====================================================================
+    }
+}
